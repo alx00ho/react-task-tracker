26052218517b7f04813d0fd2d88451a614f7ce50
alx00ho <46589739+alx00ho@users.noreply.github.com>
2023-05-26 09:21:00 -0400

    Initial Commit
    React JS Crash Course from Traversy Media

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Tasks from './components/Tasks';
+import AddTasks from './components/AddTasks';
+import { useState } from "react"
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doc Appt',
+        day: ' Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Concert',
+        day: ' May 6th at 6:30pm',
+        reminder: false,
+    },
+    {
+        id: 3,
+        text: 'Work',
+        day: ' May 25th at 4pm',
+        reminder: true,
+    }
+])
+
+// Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id));
+}
+
+// Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder } : task ))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <AddTasks />
+      {tasks.length > 0 ?
+      (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        'No Tasks To Show'
+        )}
     </div>
   );
 }
