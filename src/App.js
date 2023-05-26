01d1ccc1ed0c4eb6f2d17b6fe1fc1f0465ee53e0
alx00ho <46589739+alx00ho@users.noreply.github.com>
2023-05-26 11:23:29 -0400

    Pre-Backend
    Frontend Finished
    Production Build Tested

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AddTasks from './components/AddTasks';
 import { useState } from "react"
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -25,6 +26,13 @@ function App() {
     }
 ])
 
+// Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1;
+  const newTask = { id, ...task };
+  setTasks([...tasks, newTask]);
+}
+
 // Delete Task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id));
@@ -37,8 +45,8 @@ const toggleReminder = (id) => {
 
   return (
     <div className="container">
-      <Header />
-      <AddTasks />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTasks onAdd={addTask}/>}
       {tasks.length > 0 ?
       (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
